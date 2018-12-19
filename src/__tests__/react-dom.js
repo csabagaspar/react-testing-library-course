81e358066a253a339f3733ae83ca8ec3645b181e
csabagaspar <gaspar.csaba@gmail.com>
2018-12-19 16:04:51 +0100

    add react-dom test

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -1,13 +1,20 @@
 // 🐨 you're going to need to use React to create react elements, so import react
+import React from 'react'
 // 🐨 we're going to render the FavoriteNumber component with ReactDOM so you'll need to import react-dom
+import ReactDOM from 'react-dom'
 // 🐨 Here's your component:
 // import {FavoriteNumber} from '../favorite-number'
+import {FavoriteNumber} from '../favorite-number'
 
 test('renders a number input with a label "Favorite Number"', () => {
   // 🐨 create a div (💯 document.createElement)
+  const div = document.createElement('div')
   // 🐨 render the FavoriteNumber component to that div with ReactDOM.render
+  ReactDOM.render(<FavoriteNumber />, div)
   // 🐨 assert the input type attribute is a number
+  expect(div.querySelector('input').type).toBe('number')
   //:🐨 assert the label's text content is "Favorite Number"
+  expect(div.querySelector('label').innerHTML).toBe('Favorite Number')
 })
 
 //////// Elaboration & Feedback /////////
